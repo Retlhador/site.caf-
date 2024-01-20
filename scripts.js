cefddc3d32bebe5cc9a9a8d6cde8b07eb76d7469
Marques77 <110624478+Retlhador@users.noreply.github.com>
2024-01-20 14:03:34 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,23 @@
+<script>
+    let circulo = document.querySelector(".circulo")
+    let = imgCopo = document.querySelector(".starbucks")
+    let verde = document.querySelector(".verde")
+    let amarelo = document.querySelector(".amarelo")
+    let rosa = document.querySelector(".rosa")
+
+    verde.addEventListener("click", () => {
+        imgCopo.src = "./img/img1.png"
+        circulo.style.background ="#017043"
+    }) 
+    
+    amarelo.addEventListener("click", () => {
+        imgCopo.src = "./img/img2.png"
+        circulo.style.background ="#eb7496"
+    })
+
+    rosa.addEventListener("click", () => {
+        imgCopo.src = "./img/img3.png"
+        circulo.style.background ="#d752b1"
+    })
+   
+</script>
